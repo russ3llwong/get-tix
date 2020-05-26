733fdf1e2883d498c98d96aab45ceaade7973574
russ3llwong <russellwong97@hotmail.com>
2020-05-26 16:29:57 -0700

    Added mongo deployment for auth service and installed cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true); // trust proxy of ingress nginx
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -26,19 +34,19 @@ app.use(errorHandler);
 
 const start = async () => {
     try {
-        await mongoose.connect('mongoose://auth-mongo-srv:27017/auth', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        console.log('Connected to MongoDb');
+      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      });
+      console.log('Connected to MongoDb');
     } catch (err) {
-        console.log(err);
-    } 
+      console.error(err);
+    }
+  
+    app.listen(3000, () => {
+      console.log('Listening on port 3000!!!!!!!!');
+    });
 };
-
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
 
 start();
