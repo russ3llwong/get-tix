6640f8901b829153181c81cc74e0291e92f10729
russ3llwong <russellwong97@hotmail.com>
2020-05-26 17:27:12 -0700

    Added configurations for jwt_key env variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,6 +33,11 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY env var must be defined.')
+    }
+
     try {
       await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
         useNewUrlParser: true,
