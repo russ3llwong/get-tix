51b8b8cd0a7ed1a77de82ed6c8c631eb12fcf3dd
russ3llwong <russellwong97@hotmail.com>
2020-05-28 00:38:17 -0700

    Setup test environment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); // trust proxy of ingress nginx
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
 
@@ -50,7 +20,7 @@ const start = async () => {
     }
   
     app.listen(3000, () => {
-      console.log('Listening on port 3000!!!!!!!!');
+      console.log('Listening on port 3000!!!');
     });
 };
 
