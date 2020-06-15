6489d600f4131e8fcda4edc0d451df6da8a85fb5
russ3llwong <russellwong97@hotmail.com>
2020-06-15 16:19:42 -0700

    Implemented cancel order and unit tests for it

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { requireAuth, NotAuthorizedError, NotFoundError, OrderStatus } from '@get-tix/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // todo: publish an event 
+
+    res.status(204).send(order); 
 })
 
 export { router as deleteOrderRouter }; 
