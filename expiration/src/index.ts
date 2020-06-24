8ff851efd3e2dca845a3edaf7d43a86797f616ea
russ3llwong <russellwong97@hotmail.com>
2020-06-23 18:16:51 -0700

    Added k8s & skaffold config for expiration service and fixed NATS connection err

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -14,9 +14,10 @@ const start = async () => {
 
     try {
       await natsWrapper.connect(
-        process.env.NATS_CLIENT_ID, 
-        process.env.NATS_URL, 
-        process.env.NATS_CLUSTER_ID);
+        process.env.NATS_CLUSTER_ID,
+        process.env.NATS_CLIENT_ID,
+        process.env.NATS_URL
+      );
       natsWrapper.client.on('close', () => {
         console.log('NATS connection closed!');
         process.exit();
