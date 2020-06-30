3f2db9a8c92346113d4d3764feefccdb09ada6c9
russ3llwong <russellwong97@hotmail.com>
2020-06-30 01:27:39 -0700

    Refactored landing page, added create ticket page and ticket page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,45 @@
-const LandingPage = ({ currentUser }) => {
+import Link from 'next/link';
 
-    return currentUser ? (
-        <h1>You are signed in</h1>
-    ) : (
-        <h1>You are NOT signed in</h1>
-    );
+const LandingPage = ({ currentUser, tickets }) => {
+
+    const ticketList = tickets.map(ticket => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+                <td>
+                    <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+                        <a>View</a>
+                    </Link>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div>
+            <h1>Tickets</h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ticketList}
+                </tbody>
+            </table>
+        </div>
+    )
 };
 
 // initiated during rendering process
-LandingPage.getInitialProps = async (context) => {
-    return {}
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/tickets');
+
+    return { tickets: data };
 };
 
 export default LandingPage;
